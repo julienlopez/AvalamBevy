7d3e5546bb8f64fb2adcdf34128e8e6e67d055b5
julien lopez <julien.lopez51@gmail.com>
2023-10-21 15:34:48 +0200

    adding a basic menu

---
+++ src/game/game_plugin.rs
@@ -0,0 +1,71 @@
+use bevy::prelude::*;
+use bevy_mod_picking::prelude::*;
+
+use crate::game::{
+    board::grid_2_world,
+    board::{generate_all_positions, BoardPosition, GridPosition},
+    drag_and_drop_event::{on_drag_end, stack_pieces},
+    stack::{Piece, Stack},
+    utils::{image_path, stack_to_image_path},
+};
+use crate::gamestate::GameState;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            DefaultPlugins.set(low_latency_window_plugin()),
+            DefaultPickingPlugins,
+        )).add_systems(OnEnter(GameState::Game), game_setup)
+            // .add_systems(Update, game.run_if(in_state(GameState::Game)))
+            // .add_systems(OnExit(GameState::Game), despawn_screen::<OnGameScreen>)
+            ;
+    }
+}
+
+fn game_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // possible to use commands.spawn_batch(vec![]);
+    for grid_pos in generate_all_positions() {
+        spawn_piece_stack(&mut commands, &asset_server, &grid_pos);
+        let world_pos = grid_2_world(&grid_pos);
+        commands.spawn(SpriteBundle {
+            texture: asset_server.load(image_path("xxxxx")),
+            transform: Transform::from_xyz(world_pos.x, world_pos.y, -1.0),
+            ..default()
+        });
+    }
+}
+
+fn spawn_piece_stack(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    grid_position: &GridPosition,
+) {
+    let color = if grid_position.x % 2 == 0 {
+        Piece::Yellow
+    } else {
+        Piece::Red
+    };
+    let stack = Stack::new(vec![color]);
+    let board_position = BoardPosition::from_grid_pos(grid_position);
+    let world_pos = board_position.world_pos;
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load(stack_to_image_path(&stack)),
+            transform: Transform::from_xyz(world_pos.x, world_pos.y, 0.0),
+            ..default()
+        },
+        board_position,
+        stack,
+        PickableBundle::default(), // <- Makes the mesh pickable.
+        On::<Pointer<DragStart>>::target_insert(Pickable::IGNORE), // Disable picking
+        On::<Pointer<DragEnd>>::run(on_drag_end),
+        On::<Pointer<Drag>>::target_component_mut::<Transform>(|drag, transform| {
+            transform.translation.x += drag.delta.x;
+            transform.translation.y -= drag.delta.y;
+            transform.translation.z = 1.0;
+        }),
+        On::<Pointer<Drop>>::run(stack_pieces),
+    ));
+}
---
+++ src/menu/menu_plugin.rs
@@ -0,0 +1,173 @@
+use bevy::app::AppExit;
+use bevy::prelude::*;
+
+use crate::gamestate::GameState;
+
+pub struct MenuPlugin;
+
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Menu), menu_setup)
+            .add_systems(
+                Update,
+                (menu_action, button_system).run_if(in_state(GameState::Menu)),
+            )
+            .add_systems(OnExit(GameState::Menu), despawn_screen::<OnMainMenuScreen>);
+    }
+}
+
+const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const HOVERED_PRESSED_BUTTON: Color = Color::rgb(0.25, 0.65, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+// Tag component used to tag entities added on the main menu screen
+#[derive(Component)]
+struct OnMainMenuScreen;
+
+fn menu_setup(mut commands: Commands) {
+    let button_style = Style {
+        width: Val::Px(250.0),
+        height: Val::Px(65.0),
+        margin: UiRect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+    let button_text_style = TextStyle {
+        font_size: 40.0,
+        color: TEXT_COLOR,
+        ..default()
+    };
+
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            OnMainMenuScreen,
+        ))
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: Color::CRIMSON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Display the game name
+                    parent.spawn(
+                        TextBundle::from_section(
+                            "Avalam Bevy",
+                            TextStyle {
+                                font_size: 80.0,
+                                color: TEXT_COLOR,
+                                ..default()
+                            },
+                        )
+                        .with_style(Style {
+                            margin: UiRect::all(Val::Px(50.0)),
+                            ..default()
+                        }),
+                    );
+
+                    // Display three buttons for each action available from the main menu:
+                    // - new game
+                    // - quit
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style.clone(),
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::Play,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle::from_section(
+                                "New Game",
+                                button_text_style.clone(),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style,
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::Quit,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle::from_section("Quit", button_text_style));
+                        });
+                });
+        });
+}
+
+// Tag component used to mark which setting is currently selected
+#[derive(Component)]
+struct SelectedOption;
+
+// This system handles changing all buttons color based on mouse interaction
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, Option<&SelectedOption>),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, selected) in &mut interaction_query {
+        *color = match (*interaction, selected) {
+            (Interaction::Pressed, _) | (Interaction::None, Some(_)) => PRESSED_BUTTON.into(),
+            (Interaction::Hovered, Some(_)) => HOVERED_PRESSED_BUTTON.into(),
+            (Interaction::Hovered, None) => HOVERED_BUTTON.into(),
+            (Interaction::None, None) => NORMAL_BUTTON.into(),
+        }
+    }
+}
+
+// All actions that can be triggered from a button click
+#[derive(Component)]
+enum MenuButtonAction {
+    Play,
+    Quit,
+}
+
+fn menu_action(
+    interaction_query: Query<
+        (&Interaction, &MenuButtonAction),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut app_exit_events: EventWriter<AppExit>,
+    mut game_state: ResMut<NextState<GameState>>,
+) {
+    for (interaction, menu_button_action) in &interaction_query {
+        if *interaction == Interaction::Pressed {
+            match menu_button_action {
+                MenuButtonAction::Quit => app_exit_events.send(AppExit),
+                MenuButtonAction::Play => {
+                    game_state.set(GameState::Game);
+                }
+            }
+        }
+    }
+}
+
+// Generic system that takes a component as a parameter, and will despawn all entities with that component
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in &to_despawn {
+        commands.entity(entity).despawn_recursive();
+    }
+}
